9d2e7b06a283e1e4d973d02c27c942c4257ffda0
vishal-9368 <vshalsha@1234@gmail.com>
2024-10-17 08:44:43 +0530

    vishal changes

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log("server listening on port 8080");
 });
