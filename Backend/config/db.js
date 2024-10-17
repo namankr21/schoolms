7b91bc2ccf232e97a93875991f30834fd0d600ce
Naman Kumar <jnaman1kumar@gmail.com>
2024-10-17 10:08:45 +0530

    adding school models

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,4 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/schoolms');
 
+const dbConnection = () => {
+    mongoose
+      .connect(
+        "mongodb://127.0.0.1:27017/schoolms"
+      )
+      .then(() => console.log("Database Connection Successfull"))
+      .catch((err) =>
+        console.log("Failed To Connect With Database, \nReason : " + err.message)
+      );
+  };
+  
+  module.exports = dbConnection;
+  
--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+const dbConnection = require('./config/db.js')
 
+dbConnection();
 
 // middlewares
 
